5e7fc23288d436ae2a3c36f924182389723302a5
JoonasSuutari <suutarijoonas@gmail.com>
2017-09-13 12:07:28 +0300

    For loop task one

---
+++ silmukka-1/silmukka-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace silmukka_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f=1, num;
+
+            Console.Write("Syötä numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.Write("Tulos: {0}", num);
+            }
+
+            else if (num < 0)
+            {
+                Console.Write("Määrittelemätön");
+            }
+
+            else
+            {
+                for (i = 1; i <= num; i++)
+                    f = f * i;
+
+                Console.Write("Tulos: {0}", f);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
