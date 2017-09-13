49dd15a0de3df074893a81847bebf3d6048141a5
JoonasSuutari <suutarijoonas@gmail.com>
2017-09-13 09:20:49 +0300

    Added task five

---
+++ tehtava-5/tehtava-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tehtava_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
