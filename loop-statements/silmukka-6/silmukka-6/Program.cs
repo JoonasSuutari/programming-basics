e3082094ffb9bd1ff0718918f650ab489340666b
JoonasSuutari <suutarijoonas@gmail.com>
2017-09-18 14:52:26 +0300

    Added tasks 6 and 7

---
+++ loop-statements/silmukka-6/silmukka-6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace silmukka_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double j = 0;
+
+            for (int i = 1; i <= 10; i++)
+                {
+                    j = Math.Round(Math.Sqrt(i), 2);
+                    Console.WriteLine("Luvun {0} neliöjuuri on {1}\n", i, j);
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ loop-statements/silmukka-7/silmukka-7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace silmukka_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+
+            Console.Write("Syötä luku jonka kertotaulu lasketaan: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n");
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} x {1} = {2} \n", n, i, n * i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
